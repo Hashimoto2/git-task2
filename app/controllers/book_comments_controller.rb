3c1ac70a754eef30368ab63f14fc60d4adec06d1
EC2 Default User <ec2-user@ip-172-31-84-97.ec2.internal>
2020-11-07 06:22:20 +0000

    応用課題6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,25 @@
 class BookCommentsController < ApplicationController
   def create
-    @book = Book.find(params[:book_id])
+    # @book = Book.find(params[:book_id])
+    # comment = current_user.book_comments.new(book_comment_params)
+    # comment.book_id = @book.id
+    # comment.save
+    # redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id]) 
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = @book.id
     comment.save
-    redirect_back(fallback_location: root_path)
+    # respond_to do |format|
+    #   format.html { redirect_to books_path(params[:book_id])  }
+    #   format.json
+    # end 
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     comment = current_user.book_comments.find_by(id: params[:id], book_id: @book.id)
     comment.destroy
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
   end
   
   private
